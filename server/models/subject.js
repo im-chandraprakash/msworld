528a4051e6e0e65505c7ea3c4b3900026c95eb1d
manav <mnvgpl@gmail.com>
2023-04-27 18:40:16 +0530

    accepted current changes

--- server/models/subject.js
+++ server/models/subject.js
@@ -1,21 +1,9 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
 const subjectSchema = mongoose.Schema({
     id: {
         type:Number,
         required:true,
-=======
-const subject = mongoose.Schema({
-    id: {
-        typer: Number,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true,
-        lowercase: true
->>>>>>> 2633f8fe3de3ae585a350df6aca12bcbcfc36037
     },
     subject: {
         type: String,
--- server/models/topic.js
+++ server/models/topic.js
@@ -2,18 +2,6 @@ const mongoose = require("mongoose");
 
 const topicSchema = new mongoose.Schema({
     id: {
-<<<<<<< HEAD
-=======
-        typer: Number,
-        required: true
-    },
-    name:{
-        type: String,
-        required: true,
-        lowercase: true
-    },
-    subject_id : {
->>>>>>> 2633f8fe3de3ae585a350df6aca12bcbcfc36037
         type: Number,
         required: true,
     },
